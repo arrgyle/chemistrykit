11d000ae41c0caa3effb008138c85cf7a7feebef
jwieringa <jason@arrgyle.com>
2013-01-15 17:38:30 -0800

    uncommented code to test how things are working"

--- lib/chemistrykit/cli/cli.rb
+++ lib/chemistrykit/cli/cli.rb
@@ -2,18 +2,13 @@ require "thor"
 require 'rspec'
 require 'chemistrykit/cli/generators'
 require 'chemistrykit/cli/new'
-require 'chemistrykit/cli/brew'
 
 module ChemistryKit
   module CLI
     class CKitCLI < Thor
 
-      # default_task :brew
-
       register(ChemistryKit::CLI::Generate, 'generate', 'generate [GENERATOR] [NAME]', 'generates something')
       register(ChemistryKit::CLI::New, 'new', 'new [NAME]', 'Creates a new ChemistryKit project')
-      register(ChemistryKit::CLI::Brew, 'brew', 'brew [TAG]', 'Runs Chemistrykit')
-
 
       # TODO: Pass tags from thor to filter_run_excluding in rspec_config
       # TODO: Need to decide what the names of scripts will be and where they live
@@ -23,33 +18,38 @@ module ChemistryKit
       # TODO: Accept and pass a tag to rspec_config
       desc 'brew', 'Run ChemistryKit'
       def brew
-        #def log_timestamp
-        #  Time.now.strftime("%Y-%m-%d-%H-%M-%S")
-        #end
+        # TODO: Pull out these requires from the method
+        require 'chemistrykit/config' # This is requiring a a file that's available in userland. How to make that more clear?
+        require 'chemistrykit/shared_context'
+        require "#{Dir.getwd}/spec/helpers/spec_helper"
 
-        #def exit_code
-        #  RSpec::Core::Runner.run(Dir.glob(File.join(Dir.getwd, 'spec', '**/*_spec.rb')))
-        #end
+        def log_timestamp
+          Time.now.strftime("%Y-%m-%d-%H-%M-%S")
+        end
 
-        #FileUtils.makedirs(File.join(Dir.getwd, 'logs', log_timestamp))
+        def exit_code
+          RSpec::Core::Runner.run(Dir.glob(File.join(Dir.getwd, 'spec', '**/*_spec.rb')))
+        end
 
-        #ENV['CI_REPORTS'] = File.join(Dir.getwd, 'logs', log_timestamp)
-        #ENV['CI_CAPTURE'] = CHEMISTRY_CONFIG['chemistrykit']['capture_output'] ? 'on' : 'off'
+        FileUtils.makedirs(File.join(Dir.getwd, 'logs', log_timestamp))
 
-        # if RUBY_PLATFORM.downcase.include?("mswin")
-        #   require 'win32/dir'
+        ENV['CI_REPORTS'] = File.join(Dir.getwd, 'logs', log_timestamp)
+        ENV['CI_CAPTURE'] = CHEMISTRY_CONFIG['chemistrykit']['capture_output'] ? 'on' : 'off'
 
-        #   if Dir.junction?(File.join(Dir.getwd, 'logs', 'latest'))
-        #     File.delete(File.join(Dir.getwd, 'logs', 'latest'))
-        #   end
-        #   Dir.create_junction(File.join(Dir.getwd, 'logs', 'latest'), File.join(Dir.getwd, 'logs', log_timestamp))
-        # else
-        #   if File.symlink?(File.join(Dir.getwd, 'logs', 'latest'))
-        #     File.delete(File.join(Dir.getwd, 'logs', 'latest'))
-        #   end
-        #   File.symlink(File.join(Dir.getwd, 'logs', log_timestamp), File.join(Dir.getwd, 'logs', 'latest'))
-        # end
-        # exit_code
+        if RUBY_PLATFORM.downcase.include?("mswin")
+          require 'win32/dir'
+
+          if Dir.junction?(File.join(Dir.getwd, 'logs', 'latest'))
+            File.delete(File.join(Dir.getwd, 'logs', 'latest'))
+          end
+          Dir.create_junction(File.join(Dir.getwd, 'logs', 'latest'), File.join(Dir.getwd, 'logs', log_timestamp))
+        else
+          if File.symlink?(File.join(Dir.getwd, 'logs', 'latest'))
+            File.delete(File.join(Dir.getwd, 'logs', 'latest'))
+          end
+          File.symlink(File.join(Dir.getwd, 'logs', log_timestamp), File.join(Dir.getwd, 'logs', 'latest'))
+        end
+        exit_code
       end
     end
   end
