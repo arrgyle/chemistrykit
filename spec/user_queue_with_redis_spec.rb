6dc09d3b225e8415659db8f6cd58e284fdb55d9b
Dave Haeffner <dave@arrgyle.com>
2013-08-25 02:16:22 +0000

    Code quality check

--- spec/user_queue_with_redis_spec.rb
+++ spec/user_queue_with_redis_spec.rb
@@ -5,7 +5,7 @@ require 'redis'
 
 describe 'User Queue with Redis Example' do
 
-  #GOAL:
+  # GOAL:
   ## A user queue that works across tests
   ## and contains type and credentials
   ### (email, password, name) for each account
