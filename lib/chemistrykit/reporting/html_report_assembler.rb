78ae1bf4de978c4fff6c41ccf57b68ad03e8f704
Lindsay Landry <lindsay@animoto.com>
2015-10-16 17:27:31 -0400

    change timestamp attribute to not convert to string

--- lib/chemistrykit/reporting/html_report_assembler.rb
+++ lib/chemistrykit/reporting/html_report_assembler.rb
@@ -29,7 +29,7 @@ module ChemistryKit
           doc = Nokogiri.XML(open(file))
 
           doc.xpath('//testcase').each do |testcase|
-            time_end = DateTime.parse(testcase.attr('timestamp').to_s).to_time.to_f
+            time_end = DateTime.parse(testcase.attr('timestamp')).to_time.to_f
             time_start = time_end - testcase.attr('time').to_f
             max_time = time_end if time_end > max_time
             min_time = time_start if time_start < min_time
